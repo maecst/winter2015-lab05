6bc79d399a0386215c9a1b3ea940650f8d77d2f0
Mae Yee <mae.cst@hotmail.com>
2015-02-17 22:08:15 -0800

    * added a link to allow a new quote to be added * added form for quote addition * added form handler
    (up to page 48)
    Signed-off-by:Mae Yee <mae.cst@hotmail.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,6 +9,7 @@ class Admin extends Application {
     
     function __construct() {
         parent::__construct();
+        $this->load->helper('formfields');
     }
     
     function index() {
@@ -17,7 +18,29 @@ class Admin extends Application {
         $this->data['quotes'] = $this->quotes->all();
         $this->render();
     }
+    
+    // add a new quotation
+    function add() {
+       $quote = $this->quotes->create();
+       $this->present($quote);
+    }
+    
+    // present a quotation for adding/editing function
+    function present($quote) {
+        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
+        $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
+        $this->data['fwhat'] = makeTextField('The Quote', 'what', $quote->what);
+        $this->data['pagebody'] = 'quote_edit';
+        
+        $this->data['fsubmit'] = makeSubmitButton(
+                'Process Quote', 
+                "Click here to validate the quotation idea", 
+                'btn-success');
+        
+        $this->render();
+    }
 }
 
 /* End of file Admin.php */
-/* Location: application/controllers/Admin.php */
+/* Location: application/controllers/Admin.thphp */
